9cb09862a865046878ee982d53793f4847da9222
southernwind <testakina@gmail.com>
2023-04-12 23:04:19 +0900

    pc買い替え

--- HomeAutomation/Program.cs
+++ HomeAutomation/Program.cs
@@ -6,10 +6,10 @@ var content = new StringContent(@"{""on"":false}");
 while (true) {
     // やること増えてきたらやり方考える。
     Thread.Sleep(new TimeSpan(0, 0, 10));
-    var reply = await ping.SendPingAsync("9700k.localnet");
+    var reply = await ping.SendPingAsync("13700k.localnet");
     if (reply.Status != IPStatus.Success) {
         Thread.Sleep(new TimeSpan(0, 0, 20));
-        reply = await ping.SendPingAsync("9700k.localnet");
+        reply = await ping.SendPingAsync("13700k.localnet");
         if (reply.Status != IPStatus.Success) {
             await httpClient.PutAsync("http://hue-bridge.localnet/api/lfRks8huc0jr-AIaa0l9iFdbatH38yPCsvEqUoh1/lights/1/state", content);
             await httpClient.PutAsync("http://hue-bridge.localnet/api/lfRks8huc0jr-AIaa0l9iFdbatH38yPCsvEqUoh1/lights/2/state", content);
